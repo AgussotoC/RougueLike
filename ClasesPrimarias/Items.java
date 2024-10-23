8e8c02a06f326d2d69ed1c0c14dc44ecdc7eda49
Brandon Palacios Delgado <brandonpalaciosdelgado65@gamil.com>
2024-10-23 16:50:40 -0600

    hola agregue esto

--- ClasesPrimarias/Items.java
+++ ClasesPrimarias/Items.java
@@ -5,11 +5,8 @@ public class Items
     String nombre;
     double efecto;
     String descripcion;
-<<<<<<< HEAD
-    int num;
-=======
     int a = 1;
->>>>>>> e632580c1411dbfdc6e4d09d75194c45a60d6ac6
+    int i = 6;
     
     public Items(String nombre, double efecto, String descripcion){
         this.nombre = nombre;
